12a7dbd23ab23b37dfac86584d24f95c87cb914d
KeerthiB25 <keerthidas18@gmail.com>
2024-10-22 19:35:57 -0500

    test

---
+++ src/main/java/com/ttu/blackboard/ttudetails/UbookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ttu.blackboard.ttudetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbookBackendApplication.class, args);
+    }
+
+}
