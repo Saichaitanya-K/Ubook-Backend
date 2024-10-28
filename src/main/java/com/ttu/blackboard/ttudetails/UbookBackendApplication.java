2eb5927c77d6ff9ae40b95c49d4d6ced1d3a4e10
KeerthiB25 <keerthidas18@gmail.com>
2024-10-27 20:58:44 -0500

    ubook db changes

--- src/main/java/com/ttu/blackboard/ttudetails/UbookBackendApplication.java
+++ src/main/java/com/ttu/blackboard/ttudetails/UbookBackendApplication.java
@@ -2,9 +2,10 @@ package com.ttu.blackboard.ttudetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UbookBackendApplication {
+public class UbookBackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(UbookBackendApplication.class, args);
